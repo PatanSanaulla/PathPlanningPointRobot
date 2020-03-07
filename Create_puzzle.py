d7ac51dea5efaf47008d803db9c8be7f498461f7
PatanSanaulla <spatankhan@gmail.com>
2020-03-06 20:19:32 -0800

    updating the animation code

--- Create_puzzle.py
+++ Create_puzzle.py
@@ -16,17 +16,21 @@ startColor = (0, 255, 0)
 goalColor = (255, 0, 0)
 obstacleColor = (0, 0, 0)
 pathColor = (0, 0, 255)
-
 puzzleMap = pygame.display.set_mode((MAX_X, MAX_Y))
 puzzleMap.fill((255, 255, 255))
 
-pygame.draw.circle(puzzleMap, obstacleColor, (225,50), 25)
+def startAnimation():
+	#puzzleMap = pygame.display.set_mode((MAX_X, MAX_Y))
+	#puzzleMap.fill((255, 255, 255))
 
-pygame.draw.ellipse(puzzleMap, obstacleColor, (110, 80, 80, 40))
+	pygame.draw.circle(puzzleMap, obstacleColor, (225,50), 25)
 
-pygame.draw.polygon(puzzleMap, obstacleColor, ((95,170),(100,161),(35,124),(30,133)))
-pygame.draw.polygon(puzzleMap, obstacleColor, ((20,80),(50,50),(75,80),(100,50),(75,15),(25,15)))
-pygame.draw.polygon(puzzleMap, obstacleColor, ((225,190),(250,175),(225,160),(200,175)))
+	pygame.draw.ellipse(puzzleMap, obstacleColor, (110, 80, 80, 40))
+
+	pygame.draw.polygon(puzzleMap, obstacleColor, ((95,170),(100,161),(35,124),(30,133)))
+	pygame.draw.polygon(puzzleMap, obstacleColor, ((20,80),(50,50),(75,80),(100,50),(75,15),(25,15)))
+	pygame.draw.polygon(puzzleMap, obstacleColor, ((225,190),(250,175),(225,160),(200,175)))
+	pygame.display.update()
 
 
 def updateTheStep(position, color):
@@ -87,7 +91,7 @@ def inObs4(pos):
 		return True
 	else:
 		x, y = pos[0], pos[1]
-		if ((x - 225) ** 2 + (y - 50) ** 2 <= 225):
+		if ((x - 225) ** 2 + (y - 150) ** 2 <= 25 ** 2):
 			obs4_pts.add(pos)
 			return True
 		else:
